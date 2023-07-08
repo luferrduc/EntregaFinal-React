774d9f0fd12da5c2e281c8f5119d809b6aad8294
luferr <luciano.ferrando94@gmail.com>
2023-07-08 17:34:28 -0400

    Colocar header dentro de un section

--- src/components/Item/ItemListContainer.jsx
+++ src/components/Item/ItemListContainer.jsx
@@ -2,13 +2,13 @@
 
 const ItemListContainer = ({ grettings }) => {
   return (
-    <> 
+    <section>
       <header className="text-center">
         <h3 className="font-bold text-3xl">
           {grettings}
         </h3>
       </header>
-    </>
+    </section>
   )
 }
 
