8cef64a44c91fc0b0f8f36e4db03aeb1729eeba8
luferr <luciano.ferrando94@gmail.com>
2023-07-18 00:03:05 -0400

    Letras más chicas en el menu

--- src/components/Menu/MenuItems.jsx
+++ src/components/Menu/MenuItems.jsx
@@ -3,9 +3,9 @@
 const MenuList = () => {
   return (
     <ul className="flex gap-4 items-center">
-        <a href="#" className="font-bold text-xl text-white hover:underline ease-in-out transition-all duration-[0.4s]">Home</a>
-        <a href="#" className="font-bold text-xl text-white hover:underline ease-in-out transition-all duration-[0.4s]">Servicios</a>
-        <a href="#" className="font-bold text-xl text-white hover:underline ease-in-out transition-all duration-[0.4s]">Contacto</a>
+        <a href="#" className="font-bold text-lg text-white hover:underline ease-in-out transition-all duration-[0.4s]">Home</a>
+        <a href="#" className="font-bold text-lg text-white hover:underline ease-in-out transition-all duration-[0.4s]">Servicios</a>
+        <a href="#" className="font-bold text-lg text-white hover:underline ease-in-out transition-all duration-[0.4s]">Contacto</a>
     </ul>
   )
 }
