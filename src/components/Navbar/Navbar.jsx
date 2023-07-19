2f22aa86d74a701726e39b4b530ee00bce18fea8
luferr <luciano.ferrando94@gmail.com>
2023-07-18 23:28:52 -0400

    Nuevo logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import CartWidget from "../Cart/CartWidget"
 import MenuItems from "../Menu/MenuItems"
-import Logo from "./Logo"
+// import Logo from "./Logo.svg"
+import Logo from '../../assets/karakura.svg'
 
 
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between p-5 bg-slate-800 shadow-md w-full">
-        <Logo />
+        <Logo width="30" height="30" />
         <MenuItems />
         <CartWidget />
     </nav>
