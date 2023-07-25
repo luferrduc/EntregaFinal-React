3fc63d0cd6574e2b39ae70be17b4b54d3ee9df1f
luferr <luciano.ferrando94@gmail.com>
2023-07-24 22:41:04 -0400

    Item count styles

--- src/components/Item/ItemCount.jsx
+++ src/components/Item/ItemCount.jsx
@@ -1,7 +1,14 @@
 
 
-export const ItemCount = () => {
+export const ItemCount = ({stock, onAdd}) => {
   return (
-    <div>ItemCount</div>
+    <div className="flex flex-col gap-3 items-center">
+      <div className="flex gap-3 items-center justify-center">
+        <button className="bg-cyan-700 text-white px-5 py-1 rounded-lg font-semibold text-lg hover:bg-cyan-700/70">-</button>
+        <span className="text-white">1</span>
+        <button className="bg-cyan-700 text-white px-5 py-1 rounded-lg font-semibold text-lg hover:bg-cyan-700/70">+</button>
+      </div>
+      <button className="block text-white bg-cyan-950 px-8 py-2 rounded-lg hover:bg-cyan-950/70">Agregar</button>
+    </div>
   )
 }
